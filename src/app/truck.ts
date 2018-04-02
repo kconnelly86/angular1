bd5bf533f52342fca17480c73a830ef5845064e2
kconnelly86 <kconnelly86@yahoo.com>
2018-04-02 13:45:14 -0500

    created truck.ts to make a class for the trucks in src/app and gave it name and id properties. Also added a heroes compenent view and class displaying the Id and the name of the truck in trucks.comp.ts, showed the object by displaying it in the trucks.comp.html.

---
+++ src/app/truck.ts
@@ -0,0 +1,4 @@
+export class Truck {
+    id: 1;
+    name: string;
+}
--- src/app/trucks/trucks.component.ts
+++ src/app/trucks/trucks.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Truck } from '../truck';
 @Component({
   selector: 'app-trucks',
   templateUrl: './trucks.component.html',
   styleUrls: ['./trucks.component.css']
 })
 export class TrucksComponent implements OnInit {
-
+truck: Truck = {
+  id: 1,
+  name: 'Central BBQ'
+};
   constructor() { }
 
   ngOnInit() {
