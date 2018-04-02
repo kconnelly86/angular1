a1aac13d42aff60fb178db6db08f584779e00d2b
kconnelly86 <kconnelly86@yahoo.com>
2018-04-02 14:38:17 -0500

    wo way binding inot trucks.comp.html with ngmodel, then went into app.module.ts and imported 'formsMudule' and added it to the list of imports.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -12,7 +13,9 @@ import { TrucksComponent } from './trucks/trucks.component';
     TrucksComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
