be2992ac768b6d5d97e7e2d7f88d77a1909cfebf
kconnelly86 <kconnelly86@yahoo.com>
2018-05-21 17:09:57 -0500

    angular1, amended the detail.comp.ts imoport ln 1 to reflect input, then added the input decorator to the export on the same file.

--- src/app/truck-detail/truck-detail.component.ts
+++ src/app/truck-detail/truck-detail.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Truck } from '../truck';
 @Component({
   selector: 'app-truck-detail',
   templateUrl: './truck-detail.component.html',
   styleUrls: ['./truck-detail.component.css']
 })
 export class TruckDetailComponent implements OnInit {
-
+  @Input() truck: Truck;
   constructor() { }
 
   ngOnInit() {
