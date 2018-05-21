5852f219a9a0375bc78f565676f7f39708cf1aae
kconnelly86 <kconnelly86@yahoo.com>
2018-05-21 16:54:25 -0500

    created the truck-deatil component to keep app maintainable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TrucksComponent } from './trucks/trucks.component';
+import { TruckDetailComponent } from './truck-detail/truck-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TrucksComponent
+    TrucksComponent,
+    TruckDetailComponent
   ],
   imports: [
     BrowserModule,
