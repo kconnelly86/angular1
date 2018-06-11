1f6805d1f568e3fd460d628152b9a38f0961d0d3
kconnelly86 <kconnelly86@yahoo.com>
2018-06-11 18:08:55 -0500

    rearranged the order of the imports to see if that would change anything and it did not

--- src/app/truck.service.ts
+++ src/app/truck.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 import { Truck } from './truck';
 import { TRUCKS } from './mock-trucks';
-import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 
 @Injectable()
