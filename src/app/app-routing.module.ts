9d98c9b471e25f559734fbdbf2d392cf46d9bef1
kconnelly86 <kconnelly86@yahoo.com>
2018-06-17 18:05:18 -0500

    edited the trucks.component ot reflect the correct name of the import in app-routing. module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {TrucksComponent} from './trucks/trucks.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
+import {TrucksComponent} from './trucks/trucks.component';
+import { TrucksDetailComponent }  from './trucks-detail/trucks-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: TrucksDetailComponent },
   { path: 'trucks', component: TrucksComponent }
+  
+  
 ];
 
 @NgModule({
